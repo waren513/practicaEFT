4d4bef94dedcf55ac382286669c0e7b69e716ee7
waren513 <chr.contrerasf@duocuc.cl>
2026-07-01 17:04:39 -0400

    Create ejercicioEFT.py

---
+++ ejercicioEFT.py
@@ -0,0 +1,73 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+
+
+= RESTART: C:/Users/SSDD/AppData/Local/Programs/Python/Python314/ejercicioEFT.py
+hello word
+
+= RESTART: C:/Users/SSDD/AppData/Local/Programs/Python/Python314/ejercicioEFT.py
+====MENU PRINCIPAL=====
+1.-Stock por categoria
+2.-Busqueda de productos por rango de precio
+3.-Actualizar precio de producto
+4.-Agregar producto
+5.-Eliminar producto
+6.-Salir
+=======================
+
+= RESTART: C:/Users/SSDD/AppData/Local/Programs/Python/Python314/ejercicioEFT.py
+======== MENU PRINCIPAL =====================
+1.-Stock por categoria
+2.-Busqueda de productos por rango de precio
+3.-Actualizar precio de producto
+4.-Agregar producto
+5.-Eliminar producto
+6.-Salir
+=============================================
+
+= RESTART: C:/Users/SSDD/AppData/Local/Programs/Python/Python314/ejercicioEFT.py
+======== MENU PRINCIPAL =====================
+1.-Stock por categoria
+2.-Busqueda de productos por rango de precio
+3.-Actualizar precio de producto
+4.-Agregar producto
+5.-Eliminar producto
+6.-Salir
+=============================================
+>>> 1
+1
+>>> 
+>>> 
+>>> 
+= RESTART: C:/Users/SSDD/AppData/Local/Programs/Python/Python314/ejercicioEFT.py
+======== MENU PRINCIPAL =====================
+1.-Stock por categoria
+2.-Busqueda de productos por rango de precio
+3.-Actualizar precio de producto
+4.-Agregar producto
+5.-Eliminar producto
+6.-Salir
+=============================================
+>>> 1
+1
+>>> 2
+2
+>>> 6
+6
+>>> 
+= RESTART: C:/Users/SSDD/AppData/Local/Programs/Python/Python314/ejercicioEFT.py
+======== MENU PRINCIPAL =====================
+1.-Stock por categoria
+2.-Busqueda de productos por rango de precio
+3.-Actualizar precio de producto
+4.-Agregar producto
+5.-Eliminar producto
+6.-Salir
+=============================================
+>>> 4
+4
+>>> 5
+5
+>>> 6
+6
